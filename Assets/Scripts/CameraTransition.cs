1fbe7e52865f0153ef3710d1da036a4cdc9c8f50
slanden <slanden523@gmail.com>
2016-04-13 20:09:38 -0500

    Camera Transition
    -fuckin' damn lerp...don't work.

---
+++ Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTransition
+{
+    float smoothing = 0.01f;
+    void CallCamera(Vector3 toPos, Vector3 lookAt)
+    {
+        //Vector3 fromPos = Camera.main.transform.position;
+
+        //CamTransition(lookAt, fromPos, toPos);
+
+    }
+
+    public IEnumerator Transition(Transform target, Vector3 lookAt)
+    {
+        GameObject camera = Camera.main.gameObject;     // Find Main camera in Scene
+        Vector3 fromPos = camera.transform.position;
+
+         
+        camera.transform.parent = target;               // Parents Camera to target's transform to insure posistion displacement is realtive to target
+                
+
+        
+        while (Vector3.Distance(fromPos, target.position) > 1.05f)
+        {
+            camera.transform.localPosition = Vector3.Lerp(fromPos, new Vector3(0, 28f, -6f), smoothing * Time.deltaTime);
+            Camera.main.transform.LookAt(lookAt);
+
+            yield return null;
+        }
+        camera.transform.parent = null;                             // Un-Parent camera from target
+
+        //Vector3 fromPos = Camera.main.transform.position;
+
+        //while (Vector3.Distance(fromPos, toPos) > 1.05f)
+        //{
+        //    Camera.main.transform.position = Vector3.Lerp(fromPos, toPos, smoothing * Time.deltaTime);
+        //    Camera.main.transform.LookAt(lookAt);
+
+        //    yield return null;
+        //}        
+
+        Debug.Log("Transition Done");
+    }
+}
---
+++ Assets/Scripts/PlayerGroupController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerGroupController : MonoBehaviour
+{
+    CameraTransition camController = new CameraTransition();
+    List<Transform> players = new List<Transform>();
+    int currentPlayer = 0;
+
+    public Vector3 lookAtTarget;
+
+
+    void Start()
+    {
+        //add players to list
+        for(int i = 0; i < transform.childCount; ++i)
+            players.Add(transform.GetChild(i).transform);
+
+        //get the distance from the first player position to the camera
+        Vector3 distance = (Camera.main.transform.position - players[0].position);
+        distance.y = 0;
+
+        //loop through positions to find the shortest one. this tells
+        //us who the camera is behind, therefore who's turn it is
+        int index = 0;
+        foreach (Transform t in players)
+        {
+            
+            Vector3 newDistance = Camera.main.transform.position - t.position;
+            newDistance.y = 0;
+            //Debug.Log("Dist 2: " + newDistance.sqrMagnitude);
+
+            if (newDistance.sqrMagnitude < distance.sqrMagnitude)
+            {
+                distance = newDistance;
+                currentPlayer = index;
+            }
+            index++;
+        }
+
+    }
+
+    public void EndTurn()
+    {
+        
+        //next player
+        currentPlayer += 1;
+        
+        //if every player has gone, reset
+        if (currentPlayer > players.Count -1)
+            currentPlayer = 0;
+        
+        StartCoroutine(camController.Transition(
+                       players[currentPlayer], lookAtTarget));
+    }
+
+    void OnGUI()
+    {
+    }
+}
